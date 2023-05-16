20c27727a1beb98d22d5177e78c16f57e2e622b4
tooneys <ksl8719@kakao.com>
2023-05-17 00:30:31 +0900

    topbar complete

---
+++ src/routes/global/topbar.jsx
@@ -0,0 +1,55 @@
+import { Box, IconButton, useTheme } from '@mui/material';
+import { useContext } from 'react';
+import { ColorModeContext, tokens } from '../../theme';
+import InputBase from '@mui/material/InputBase';
+
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DardModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationAddOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutline';
+import SearchIcon from '@mui/icons-material/Search';
+
+const TopBar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display={'flex'} justifyContent={'space-between'} p={2}>
+      {/* Search Bar */}
+      <Box
+        display={'flex'}
+        backgroudColor={colors.primary[400]}
+        borderRadius={'3px'}
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search"></InputBase>
+        <IconButton type="button" sx={{ display: 'flex' }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/* Icon Btn */}
+      <Box display={'flex'}>
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === 'dark' ? (
+            <DardModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default TopBar;
